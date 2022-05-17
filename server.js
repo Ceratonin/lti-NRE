952db99e95e9e110d794ca21c27976562ef9a6b2
Ceratonin <alkash411@gmail.com>
2022-05-17 19:07:22 +0500

    file delete

--- server.js
+++ server.js
@@ -6,17 +6,22 @@ const server = require("http").Server(app);
 
 const indexPath = __dirname + "/index.html";
 
+const name = "Alba"
+
 app.get("/", (req, res) => {
-  res.sendFile(indexPath);
+  res.send(`Привет, чудик по имени ${name}`);
+  console.log("get")
 });
 
 app.post("/", (req, res) => {
-  res.sendFile(indexPath);
+  res.send(`Привет, чудик по имени ${name}`);
+  console.log("post")
+  console.log(req.form)
 })
 
-app.get("/lti", (req, res) => {
-  res.sendFile(indexPath);
-})
+// app.get("/lti", (req, res) => {
+//   res.sendFile(indexPath);
+// })
 
 const PORT = 9999
 
